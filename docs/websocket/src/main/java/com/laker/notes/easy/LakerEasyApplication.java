7bfeb850196c85e03d5c162f8c2f0a809208c65d
wangpc <1720453219@qq.com>
2023-03-21 09:20:55 +0800

    webssh demo

---
+++ docs/websocket/src/main/java/com/laker/notes/easy/LakerEasyApplication.java
@@ -0,0 +1,17 @@
+package com.laker.notes.easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ */
+@SpringBootApplication
+public class LakerEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LakerEasyApplication.class, args);
+    }
+
+
+
+}
